640fce8fe674ef7e549eaab9d62176d54a618d0d
pescocofino <134400750+pescocofino@users.noreply.github.com>
2024-08-14 10:34:53 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,60 @@
+ // Variaveis
+ // tipo Number (int)
+
+ const meuNumero = 6;
+ const numeroUm = 0;
+ const numeroDois = 2;
+ 
+ const soma = 3 + 6
+ console.log(soma)
+
+ const subtracao = numeroUm - numeroDois
+ console.log(subtracao)
+
+ const multiplicacao = numeroDois * 10
+ console.log(multiplicacao)
+
+ const divisao = meuNumero / numeroDois
+ console.log(divisao)
+
+ //Float ( Numero Decimal )
+
+ const numeroTres = 3.3;
+ const numeroQuatro = .5 // mesma coisa que 0.5
+
+ const operacao = numeroTres * numeroQuatro
+ console.log(operacao)
+
+ // Not a Number
+
+ const operacaoN = 'a' * meuNumero
+ console.log(operacaoN)
+
+ //concatençao
+ const novaOperacao = 'a' + meuNumero
+ console.log(novaOperacao)
+
+ // string (texto)
+
+ const meuNome = 'Luiz'
+ const frase = 'Meu nome e:'
+ const issoNaoEumNumero = '50'
+
+ const minhaFrase = meuNome + frase
+ console.log(minhaFrase)
+ 
+ const operacaoO = issoNaoEumNumero + meuNumero
+ console.log(operacaoO)
+
+ //Boolean
+ //True -> Verdadeiro
+ //False -> Falso
+
+ const comparacao = 6 === meuNumero
+ console.log(comparacao)
+
+ const comparacaoII = '6' === meuNumero
+ console.log(comparacaoII)
+
+ const comparacaoIII = '6' == meuNumero
+ console.log(comparacaoIII)
